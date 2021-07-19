92f3d55611d7ddebb1edc3e533a286add3543898
FmarshalD <kirh09764@gmail.com>
2021-07-19 09:30:35 +0700

    recent migrations

--- app/migrations/Version20210719015744.php
+++ app/migrations/Version20210719015744.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210716055123 extends AbstractMigration
+final class Version20210719015744 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20210716055123 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE ticket CHANGE Кresponse response LONGTEXT DEFAULT NULL');
+        $this->addSql('ALTER TABLE ticket DROP user_name');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE ticket CHANGE response Кresponse LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE ticket ADD user_name VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
     }
 }
